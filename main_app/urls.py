9dfbb8f72106ee243dbe51feee0076e75cf19246
shatha <sha7cs@gmail.com>
2025-10-26 20:05:20 +0300

    challengeDetail view, GET, PUT, and DELETE methods on a challenge

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import ChallengeIndex
+from .views import ChallengeIndex, ChallengeDetail
 
 urlpatterns = [
-    path('challenges/', ChallengeIndex.as_view(), name='all_challenges')
+    path('challenges/', ChallengeIndex.as_view(), name='all_challenges'),
+    path('challenges/<int:challenge_id>/', ChallengeDetail.as_view(), name='challenge_detail')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
+from django.shortcuts import get_object_or_404
+
 from .models import Challenge, Goal
 from .serializers import ChallengeSerializer
 
@@ -27,4 +29,33 @@ class ChallengeIndex(APIView):
             return Response(serializer.errors , status=status.HTTP_400_BAD_REQUEST)
         except Exception as error:
                 return Response({'error':str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-        
+
+class ChallengeDetail(APIView):
+    
+    def get(self, request, challenge_id):
+        try:
+            queryset = get_object_or_404(Challenge, id=challenge_id)
+            serializer = ChallengeSerializer(queryset)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as error:
+            return Response({'error':str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def put(self, request, challenge_id):
+        try:
+            queryset = get_object_or_404(Challenge, id=challenge_id)
+            serializer = ChallengeSerializer(queryset, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as error:
+            return Response({'error':str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def delete(self, request, challenge_id):
+        try:
+            queryset = get_object_or_404(Challenge, id=challenge_id)
+            queryset.delete()
+            
+            return Response({'message':f'challenge {challenge_id} is deleted'})
+        except Exception as error:
+            return Response({'error':str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
