25afbeaae50a0f731ee6a45721aff642e567bcdc
shatha <sha7cs@gmail.com>
2025-10-28 10:52:19 +0300

    Goal Index and Goal Detail Views

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
-from .views import ChallengeIndex, ChallengeDetail, SignUpView
+from .views import ChallengeIndex, ChallengeDetail, SignUpView,GoalIndex,GoalDetail
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('challenges/', ChallengeIndex.as_view(), name='all_challenges'),
     path('challenges/<int:challenge_id>/', ChallengeDetail.as_view(), name='challenge_detail'),
+    path('challenges/<int:challenge_id>/goals/', GoalIndex.as_view(), name='challenge_goals'),
+    path('challenges/goals/<int:goal_id>/', GoalDetail.as_view(), name='goal_detail'),
     path('login/', TokenObtainPairView.as_view(), name='login'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('signup/', SignUpView.as_view(), name="signup")
--- main_app/views.py
+++ main_app/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
 
 from .models import Challenge, UserProfile, Goal
-from .serializers import ChallengeSerializer
+from .serializers import ChallengeSerializer, GoalSerializer, ChallengeMemberSerializer
 
 User = get_user_model()
 
@@ -70,23 +70,80 @@ class SignUpView(APIView):
     permission_classes = [AllowAny]
     
     def post(self,request):
-        username = request.data.get("username")
-        email = request.data.get("email")
-        password = request.data.get("password")
-        first_name = request.data.get("first_name")
+        try:
+            username = request.data.get("username")
+            email = request.data.get("email")
+            password = request.data.get("password")
+            first_name = request.data.get("first_name")
+            
+            if not username or not email or not password or not first_name:
+                return Response({'errors':'Please fill all the sign up fields!'}, status=status.HTTP_400_BAD_REQUEST)
+            
+            if User.objects.filter(username=username).exists():
+                return Response({"error":"username already exist"})
+            
+            user = User.objects.create_user(
+                username=username, email=email, password=password,first_name=first_name
+            )
+            user_profile = UserProfile.objects.create(user=user)
+            
+            return Response(
+                {"id": user.id, "username": user.username, "email": user.email},
+                status=status.HTTP_201_CREATED,
+            )
+        except Exception as error:
+                    return Response({'error':str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+class GoalIndex(APIView):
+    permission_classes = [AllowAny]
+    
+    def get(self, request, challenge_id):
+        try:
+            queryset = Goal.objects.filter(challenge=challenge_id)
+            serializer = GoalSerializer(queryset, many=True)
+            
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as error:
+                return Response({'error':str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            
+    def post(self, request, challenge_id):
+        try:
+            serializer = GoalSerializer(data= request.data)
+            
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data,status=status.HTTP_201_CREATED)
+            return Response(serializer.errors , status=status.HTTP_400_BAD_REQUEST)
+        except Exception as error:
+                return Response({'error':str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+class GoalDetail(APIView):
+    permission_classes = [AllowAny]
+    def get(self, request, goal_id): # i just realize the challenge id is not really necessary :/ or is it
+        try:
+            queryset = get_object_or_404(Goal,id=goal_id)
+            serializer = GoalSerializer(queryset)
+            
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as error:
+                return Response({'error':str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
+    def put(self, request, goal_id):
+        try:
+            queryset = get_object_or_404(Goal, id=goal_id)
+            serializer = GoalSerializer(queryset, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as error:
+            return Response({'error':str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         
-        if not username or not email or not password or not first_name:
-            return Response({'errors':'Please fill all the sign up fields!'}, status=status.HTTP_400_BAD_REQUEST)
-        
-        if User.objects.filter(username=username).exists():
-            return Response({"error":"username already exist"})
-        
-        user = User.objects.create_user(
-             username=username, email=email, password=password,first_name=first_name
-        )
-        user_profile = UserProfile.objects.create(user=user)
-        
-        return Response(
-            {"id": user.id, "username": user.username, "email": user.email},
-            status=status.HTTP_201_CREATED,
-        )
+    def delete(self, request, goal_id):
+        try:
+            queryset = get_object_or_404(Goal, id=goal_id)
+            queryset.delete()
+            
+            return Response({'message':f'Goal {goal_id} is deleted'})
+        except Exception as error:
+            return Response({'error':str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
