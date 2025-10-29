f23941aa967253b302ce30d6bbb5155ac40355f4
shatha <sha7cs@gmail.com>
2025-10-29 18:20:29 +0300

    changed the add member class to be joining by the challenge join_code

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path, include
-from .views import ChallengeIndex, ChallengeDetail, SignUpView, GoalIndex, GoalDetail, ChallengeMemberAdd, ChallengeMemberDelete
+from .views import ChallengeIndex, ChallengeDetail, SignUpView, GoalIndex, GoalDetail, ChallengeMemberDelete,JoinChallenge
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('challenges/', ChallengeIndex.as_view(), name='all_challenges'),
     path('challenges/<int:challenge_id>/', ChallengeDetail.as_view(), name='challenge_detail'),
     path('challenges/<int:challenge_id>/goals/', GoalIndex.as_view(), name='challenge_goals'),
     path('challenges/<int:challenge_id>/goals/<int:goal_id>/', GoalDetail.as_view(), name='goal_detail'),
-    path('challenges/<int:challenge_id>/add-user/<int:user_id>/', ChallengeMemberAdd.as_view(), name='add_member'),
     path('challenges/<int:challenge_id>/delete-member/<int:member_id>/', ChallengeMemberDelete.as_view(), name='delete_member'),
+    path('challenges/join/<int:user_id>/', JoinChallenge.as_view(), name='member_joins'),
     # Auth
     path('login/', TokenObtainPairView.as_view(), name='login'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
--- main_app/views.py
+++ main_app/views.py
@@ -148,20 +148,6 @@ class GoalDetail(APIView):
         except Exception as error:
             return Response({'error':str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-class ChallengeMemberAdd(APIView):
-    permission_classes = [AllowAny]
-    
-    def post(self, request,challenge_id, user_id):
-        try:
-            # i should add some logic that checks is user already a member? then you cant assign twice
-            serializer = ChallengeMemberSerializer(data=request.data)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data,status=status.HTTP_201_CREATED)
-            return Response(serializer.errors , status=status.HTTP_400_BAD_REQUEST)
-        except Exception as error:
-            return Response({'error':str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
 # it will have thesame or similar url to the prev class but the entities are diffrent so i made a new class for it 
 class ChallengeMemberDelete(APIView):
     permission_classes = [AllowAny]
@@ -175,3 +161,18 @@ class ChallengeMemberDelete(APIView):
             return Response({'message':f'Member {member_id} is deleted from "{challenge.name}" challenge'})
         except Exception as error:
             return Response({'error':str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+class JoinChallenge(APIView):
+    permission_classes = [AllowAny]
+    def post(self, request, user_id):
+        try:
+            join_code = request.data.get('join_code') # i think it makes sense more like this that sent with url
+            challenge = get_object_or_404(Challenge, join_code=join_code) # i want to make it send a special message if challenge not find
+            serializer = ChallengeMemberSerializer(data={'challenge':challenge.id,'user':user_id})
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as error:
+            return Response({'error':str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            
